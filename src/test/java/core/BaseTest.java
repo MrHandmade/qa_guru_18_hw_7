82fec458204e15e4f17bab255947afbcfeade54e
MaximShavikin <shavikin2012@gmail.com>
2023-05-21 18:14:48 +0300

    add allure, selenoid

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -2,7 +2,15 @@ package core;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import utils.Attach;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 
@@ -12,12 +20,32 @@ public class BaseTest {
     public static void setUp() {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.screenshots = false;
-        Configuration.savePageSource = false;
         baseUrl = "https://demoqa.com/";
-        Configuration.holdBrowserOpen = true;
+        Configuration.browserVersion = "100.0";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
 
         Selenide.open(baseUrl + "automation-practice-form");
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
--- src/test/java/tests/DemoQaTest.java
+++ src/test/java/tests/DemoQaTest.java
@@ -1,10 +1,15 @@
 package tests;
 
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import core.BaseTest;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import pages.RegistrationPage;
+
+import static io.qameta.allure.Allure.step;
 import static utils.RandomDataUtils.*;
 
 
@@ -13,6 +18,8 @@ public class DemoQaTest extends BaseTest {
 
     @Test
     public void registrationFormTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         String name = getRandomName(),
                 lastName = getRandomLastName(),
                 email = getRandomEmail(),
@@ -25,35 +32,38 @@ public class DemoQaTest extends BaseTest {
                 hobbies = hobbiesValue,
                 address = getRandomAddress(),
                 state = stateValue,
-                city = setCity() ;
+                city = setCity();
 
+        step("Заполнеяем форму", () -> {
+            registrationPage.removeBanner()
+                    .setUserName(name)
+                    .setUserLastName(lastName)
+                    .setUserEmail(email)
+                    .setGender(gender)
+                    .setUserNumber(userNumber)
+                    .setBirthdayDate(dayOfBirth, monthOfBirth, yearOfBirth)
+                    .setSubject(subject)
+                    .setHobbies(hobbies)
+                    .setPicture()
+                    .setAddress(address)
+                    .setState(state)
+                    .setCity(city)
+                    .submitRegistrationForm();
+        });
 
-        registrationPage.removeBanner()
-                .setUserName(name)
-                .setUserLastName(lastName)
-                .setUserEmail(email)
-                .setGender(gender)
-                .setUserNumber(userNumber)
-                .setBirthdayDate(dayOfBirth, monthOfBirth, yearOfBirth)
-                .setSubject(subject)
-                .setHobbies(hobbies)
-                .setPicture()
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .submitRegistrationForm()
-
-                .registrationResult()
-                .registrationFormResult("Student Name", name + " " + lastName)
-                .registrationFormResult("Student Email", email)
-                .registrationFormResult("Gender", gender)
-                .registrationFormResult("Mobile", userNumber)
-                .registrationFormResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                .registrationFormResult("Subjects", subject)
-                .registrationFormResult("Hobbies", hobbies)
-                .registrationFormResult("Address", address)
-                .registrationFormResult("State and City", state + " " + city)
-                .closeModal();
+        step("Заполнеяем форму", () -> {
+            registrationPage.registrationResult()
+                    .registrationFormResult("Student Name", name + " " + lastName)
+                    .registrationFormResult("Student Email", email)
+                    .registrationFormResult("Gender", gender)
+                    .registrationFormResult("Mobile", userNumber)
+                    .registrationFormResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                    .registrationFormResult("Subjects", subject)
+                    .registrationFormResult("Hobbies", hobbies)
+                    .registrationFormResult("Address", address)
+                    .registrationFormResult("State and City", state + " " + city)
+                    .closeModal();
+        });
     }
 
 }
